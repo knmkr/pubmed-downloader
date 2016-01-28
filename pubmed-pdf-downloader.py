4dfd784afed8348ba363caa574ea4ad0101a9db9
Kensuke Numakura <knmkr3gma@gmail.com>
2016-01-28 18:02:59 +0900

    add oxford journals downloader

--- pubmed-pdf-downloader.py
+++ pubmed-pdf-downloader.py
@@ -2,69 +2,146 @@
 # -*- coding: utf-8 -*-
 
 import os
-import requests
 import urlparse
 import argparse
+
 from lxml import html
+import requests
 
-
-def _main():
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--pubmed-id', help='')
-    parser.add_argument('--dst-dir', default='.', help='')
+def main():
+    parser = argparse.ArgumentParser(description='Downlaod open access full text pdf and supplemental materials of each PubMed IDs.')
+    parser.add_argument('--pubmed-ids', nargs='+', required=True, help='PubMed IDs')
+    parser.add_argument('--dst-dir', default='.', help='destination directory')
     args = parser.parse_args()
 
-    print '[INFO] PMID:', args.pubmed_id
+    downloaders = {
+        'oxfordjournals.org': oxford_journals_downloader,
+        # 'www.ncbi.nlm.nih.gov/pmc': pmc_downloader,
+    }
 
-    # Downlaod from PMC
-    url = 'http://www.ncbi.nlm.nih.gov/pmc/articles/pmid/{pmid}/pdf'.format(pmid=args.pubmed_id)
+    for pubmed_id in args.pubmed_ids:
+        print '[INFO] Pubmed ID:', pubmed_id
 
-    print '[INFO] Try to download full text pdf from:', url
+        # Get publisher information
+        publisher_links = get_publisher_links(pubmed_id)
+
+        if not publisher_links:
+            print '[INFO] Publisher links not found'
+            continue
+
+        # Select downloader by publisher information
+        downloader = None
+        for publisher_link in publisher_links:
+            for pattern, _downloader in downloaders.items():
+                if pattern in publisher_link:
+                    downloader = _downloader
+
+        if not downloader:
+            print '[INFO] Not supported publisher'
+            continue
+
+        # Download full text and supplemental materials
+        is_downloaded = downloader(pubmed_id, args)
+
+        if not is_downloaded:
+            print '[INFO] Download failed'
+            continue
+
+def oxford_journals_downloader(pubmed_id, args):
+    '''Downlaod from OXFORD JOURNALS
+
+    E.g.
+    - open access: 23612905 25628336
+    - non-open access: 25429064
+    '''
+
+    print '[INFO] Try to download from OXFORD JOURNALS'
+
+    # FIXME *.oxfordjournals.org?
+    url = 'http://hmg.oxfordjournals.org/cgi/pmidlookup?pmid={pmid}'.format(pmid=pubmed_id)
     response = requests.get(url)
 
     if str(response.status_code).startswith('4'):
-        print '[INFO] Failed to download from PMC. Status code:', response.status_code
-        return
+        print '[ERROR] Failed. Status code:', response.status_code
+        return False
 
-        # TODO: Try to download from other resources
-        # - "FREE FINAL TEXT OXFORD JOURNALS", e.g. http://www.ncbi.nlm.nih.gov/pubmed/23612905
-        # - etc.
+    # Get link to supplemental materials
+    body = html.fromstring(response.content)
+    supplemental_url = absolute_url(response, body.xpath('//a[text()="Supplementary Data"]/@href')[0])
 
-    # TODO: DRY
-    outfile = 'PMID{pmid}.pdf'.format(pmid=args.pubmed_id)
-    dst = os.path.join(args.dst_dir, outfile)
-    with open(dst, 'wb') as fout:
-        fout.write(response.content)
-    print '[INFO] Downloaded:', dst
+    # Download full text pdf
+    pdf_url = response.url.strip('/') + '.full.pdf'
+    is_downloaded = download_file(pdf_url, os.path.join(args.dst_dir, 'PMID{pmid}.pdf'.format(pmid=pubmed_id)))
 
-    url = 'http://www.ncbi.nlm.nih.gov/pmc/articles/pmid/{pmid}'.format(pmid=args.pubmed_id)
+    if not is_downloaded:
+        return False
 
+    # Download supplemental materials
+    if not supplemental_url:
+        # FIXME:
+        print '[WARN] Supplemental materials not found'
+        return True
+
+    response = requests.get(supplemental_url)
+    body = html.fromstring(response.content)
+    supplemental_file_urls = [absolute_url(response, href) for href in body.xpath('//h2[text()="Supplementary Data"]/following-sibling::ul/li/a/@href')]
+
+    if not supplemental_file_urls:
+        print '[WARN] Link to supplemental materials exists, but files not found'
+        return False
+    else:
+        for i, supplemental_file_url in enumerate(supplemental_file_urls):
+            _, extention = os.path.splitext(supplemental_file_url)
+            download_file(supplemental_file_url, os.path.join(args.dst_dir, 'PMID{pmid}_S{i}{extention}'.format(pmid=pubmed_id, i=i+1, extention=extention)))
+
+            if not is_downloaded:
+                return False
+
+        return True
+
+def pmc_downloader(pubmed_id, args):
+    '''Downlaod from PMC
+    '''
+
+    print '[INFO] Try to download from PMC'
+
+    pdf_url = 'http://www.ncbi.nlm.nih.gov/pmc/articles/pmid/{pmid}/pdf'.format(pmid=pubmed_id)
+    print '[INFO] Download from:', pdf_url
+
+    response = requests.get(pdf_url)
+
+    if str(response.status_code).startswith('4'):
+        print '[INFO] Failed. Status code:', response.status_code
+        return False
+
+    download(response, os.path.join(args.dst_dir, 'PMID{pmid}.pdf'.format(pmid=pubmed_id)))
+
+    url = 'http://www.ncbi.nlm.nih.gov/pmc/articles/pmid/{pmid}'.format(pmid=pubmed_id)
     print '[INFO] Try to download supplementary materials from:', url
 
     response = requests.get(url)
     body = html.fromstring(response.content)
-    supplementary_material_urls = body.xpath('//h2[text()="Supplementary Material" or text()="SUPPLEMENTARY MATERIAL" or text()="Supplemental Data" or text()="SUPPLEMENTAL DATA"]/parent::node()/descendant::*/a/@href')  # FIXME
+    # FIXME
+    supplementary_material_urls = body.xpath('//h2[text()="Supplementary Material" or text()="SUPPLEMENTARY MATERIAL" or text()="Supplemental Data" or text()="SUPPLEMENTAL DATA"]/parent::node()/descendant::*/a/@href')
+
+    # TBD: use PMC API? (http://europepmc.org/RestfulWebService#suppFiles)
 
     i = 1
     for url in supplementary_material_urls:
-        if url.startswith('/'):
-            url = 'http://www.ncbi.nlm.nih.gov' + url
-
+        url = absolute_url(response, url)
         response = requests.get(url)
 
+        # Direct link to a supplementary material
         if not 'html' in response.headers.get('Content-Type'):
             _, extention = os.path.splitext(url)
-            outfile = 'PMID{pmid}_S{i}{extention}'.format(pmid=args.pubmed_id, i=i, extention=extention)
-            dst = os.path.join(args.dst_dir, outfile)
-            with open(dst, 'wb') as fout:
-                fout.write(response.content)
+            download(response, os.path.join(args.dst_dir, 'PMID{pmid}_S{i}{extention}'.format(pmid=pubmed_id, i=i, extention=extention)))
             i += 1
-            print '[INFO] Downloaded:', dst
             continue
 
+        # External link to supplementary materials
         print '[INFO] External link found:', url
-
         body = html.fromstring(response.content)
+        # FIXME
         external_urls = [link for link in body.xpath('//a/@href') if link.endswith(('.pdf',
                                                                                     '.doc',
                                                                                     '.docx',
@@ -78,21 +155,49 @@ def _main():
                                                                                     '.jpeg',
                                                                                     '.png',
                                                                                     '.tif',
-                                                                                    '.tiff',))]  # FIXME
+                                                                                    '.tiff',))]
 
         for ext_url in external_urls:
-            if ext_url.startswith('/'):
-                ext_url = urlparse.urljoin(response.url, '/') + ext_url
-
+            ext_url = absolute_url(response, ext_url)
             _, extention = os.path.splitext(ext_url)
-            outfile = 'PMID{pmid}_S{i}{extention}'.format(pmid=args.pubmed_id, i=i, extention=extention)
-            dst = os.path.join(args.dst_dir, outfile)
-            with open(dst, 'wb') as fout:
-                fout.write(response.content)
+            download(os.path.join(args.dst_dir, 'PMID{pmid}_S{i}{extention}'.format(pmid=pubmed_id, i=i, extention=extention)))
             i += 1
-            print '[INFO] Downloaded:', dst
-            continue
 
+    return True
+
+def get_publisher_links(pubmed_id):
+    '''Get publisher links from PubMed
+    '''
+
+    pubmed_url = 'http://www.ncbi.nlm.nih.gov/pubmed/{pmid}'.format(pmid=pubmed_id)
+    response = requests.get(pubmed_url)
+    body = html.fromstring(response.content)
+    links = body.xpath('//span[text()="Full text links"]/../../../../a/@href')
+    print '[INFO] Publisher links:', links
+
+    return links
+
+def absolute_url(response, relative_url):
+    base = urlparse.urljoin(response.url, '/')
+    return os.path.join(base, relative_url.lstrip('/'))
+
+def download_file(url, dst):
+    '''Save response as file to dst path
+    '''
+
+    print '[INFO] Download from:', url
+    response = requests.get(url)
+
+    # FIXME:
+    if 'html' in response.headers.get('Content-Type'):
+        print '[ERROR] Failed. Maybe not open access article'
+        return False
+
+    with open(dst, 'wb') as fout:
+        fout.write(response.content)
+    print '[INFO] Downloaded to:', dst
+
+    return True
 
 if __name__ == '__main__':
-    _main()
+    main()
