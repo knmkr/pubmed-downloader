346f9ce115b7bce44a1036a13811c82d831aa33c
Kensuke Numakura <knmkr3gma@gmail.com>
2016-02-05 17:01:11 +0900

    fix guessing extension

--- pmdl/utils.py
+++ pmdl/utils.py
@@ -63,45 +63,39 @@ def download_file(url, dst, overwrite=False):
 
 def guess_extensions(response):
     '''Guess extension by Content-Type
-
-    - cf. Office 2007 File Format MIME Types for HTTP Content Streaming
-    http://blogs.msdn.com/b/vsofficedeveloper/archive/2008/05/08/office-2007-open-xml-mime-types.aspx
     '''
 
     mime = response.headers.get('Content-Type')
 
-    ms_mime2ext = {
-        'application/msword': '.doc',
-        # 'application/msword': '.dot',
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document': '.docx',
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.template': '.dotx',
-        'application/vnd.ms-word.document.macroEnabled.12': '.docm',
-        'application/vnd.ms-word.template.macroEnabled.12': '.dotm',
-        'application/vnd.ms-excel': '.xls',
-        # 'application/vnd.ms-excel': '.xlt',
-        # 'application/vnd.ms-excel': '.xla',
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': '.xlsx',
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.template': '.xltx',
-        'application/vnd.ms-excel.sheet.macroEnabled.12': '.xlsm',
-        'application/vnd.ms-excel.template.macroEnabled.12': '.xltm',
-        'application/vnd.ms-excel.addin.macroEnabled.12': '.xlam',
-        'application/vnd.ms-excel.sheet.binary.macroEnabled.12': '.xlsb',
-        'application/vnd.ms-powerpoint': '.ppt',
-        # 'application/vnd.ms-powerpoint': '.pot',
-        # 'application/vnd.ms-powerpoint': '.pps',
-        # 'application/vnd.ms-powerpoint': '.ppa',
-        'application/vnd.openxmlformats-officedocument.presentationml.presentation': '.pptx',
-        'application/vnd.openxmlformats-officedocument.presentationml.template': '.potx',
-        'application/vnd.openxmlformats-officedocument.presentationml.slideshow': '.ppsx',
-        'application/vnd.ms-powerpoint.addin.macroEnabled.12': '.ppam',
-        'application/vnd.ms-powerpoint.presentation.macroEnabled.12': '.pptm',
-        # 'application/vnd.ms-powerpoint.presentation.macroEnabled.12': '.potm',
-        'application/vnd.ms-powerpoint.slideshow.macroEnabled.12': '.ppsm'
+    # ugly hardcoding but to use '.ppt' instead of '.pwz' etc., define MIME Types for MS files
+    mime2ext = {
+        # cf. Office 2007 File Format MIME Types for HTTP Content Streaming
+        # http://blogs.msdn.com/b/vsofficedeveloper/archive/2008/05/08/office-2007-open-xml-mime-types.aspx
+        'application/msword': ['.doc', '.dot'],
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document': ['.docx'],
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.template': ['.dotx'],
+        'application/vnd.ms-word.document.macroEnabled.12': ['.docm'],
+        'application/vnd.ms-word.template.macroEnabled.12': ['.dotm'],
+        'application/vnd.ms-excel': ['.xls', '.xlt', '.xla'],
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx'],
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.template': ['.xltx'],
+        'application/vnd.ms-excel.sheet.macroEnabled.12': ['.xlsm'],
+        'application/vnd.ms-excel.template.macroEnabled.12': ['.xltm'],
+        'application/vnd.ms-excel.addin.macroEnabled.12': ['.xlam'],
+        'application/vnd.ms-excel.sheet.binary.macroEnabled.12': ['.xlsb'],
+        'application/vnd.ms-powerpoint': ['.ppt', '.pot', '.pps', '.ppa'],
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation': ['.pptx'],
+        'application/vnd.openxmlformats-officedocument.presentationml.template': ['.potx'],
+        'application/vnd.openxmlformats-officedocument.presentationml.slideshow': ['.ppsx'],
+        'application/vnd.ms-powerpoint.addin.macroEnabled.12': ['.ppam'],
+        'application/vnd.ms-powerpoint.presentation.macroEnabled.12': ['.pptm', '.potm'],
+        'application/vnd.ms-powerpoint.slideshow.macroEnabled.12': ['.ppsm'],
+
+        # '.jpg' instead of '.jpe' etc.
+        'image/jpeg': ['.jpg', '.jpe', '.jpeg', '.jfif'],
+        'text/plain': ['.txt', '.ksh', '.pl', '.bat', '.h', '.c', '.asc', '.text', '.pm', '.el', '.cc', '.hh', '.cxx', '.hxx', '.f90', '.conf', '.log'],
     }
 
-    ext = ms_mime2ext.get(mime) or []
-    if ext:
-        return ext
+    ext = mime2ext.get(mime) or mimetypes.guess_all_extensions(mime)
 
-    ext = mimetypes.guess_all_extensions(mime)
     return ext
